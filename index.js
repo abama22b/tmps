10b7a3b06cbd6621d2f61bbb1d4a7e1e5b9d8080
AbIgneIgnem <pppp43777@gmail.com>
2022-05-28 14:54:20 +0300

    make it better

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ let db = require('quick.db');
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+var express = require('express');
+var app = express();
 
-
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+	res.send('hello world');
+});
+app.listen(process.env.PORT || 3000)
 const client = new Client({
 	intents: [
 		Intents.FLAGS.GUILDS,
